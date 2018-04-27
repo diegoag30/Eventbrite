c9d77ca5bd8a81c67ab33a5c7cca90f6d6cfad99
diego <diego.pablo.aguilar@gmail.com>
2018-04-27 16:16:33 -0300

    Agregada primera version funcion me

--- game.py
+++ game.py
@@ -1,11 +1,30 @@
+from random import randint as rn
+
 # Definir la clase
 
 
 class Game():
-    def __init__(self, number):
-        self.number = number
+    def __init__(self, minimum, maximum):
+        self.minimum = int(minimum)
+        self.maximum = int(maximum)
 
+     # Busca la mitad entre dos numeros, lo cual acelerara el proceso de busqueda.
+    def mean(self):
+        mean_btw = (self.minimum+self.maximum)/2
+        return mean_btw
 
-# Crear funcion ME
-
-# Crear funcion Guess
+     # Funcion me, adivina numero que esta pensando el usuario
+    def me(self):
+        median_number = self.mean()
+        answer = raw_input("Your number is " + str(median_number) + " ? ")
+        if answer == "+":
+            self.minimum = median_number
+            self.me()
+        elif answer == "-":
+            self.maximum = median_number
+            self.me()
+        elif answer == "=":
+            print("Awesome! your number is " + str(median_number) + " !!")
+        else:
+            print("Sorry, that's not an answer, please answer using + , - or = ")
+            self.me()
--- game_eventbrite.py
+++ game_eventbrite.py
@@ -1,6 +1,25 @@
 # Llamar funciones de clase Game
 import game
 
-diego = game.Game("100")
 
-print(diego.number)
+diego = game.Game("0", "100")
+
+diego.me()
+
+
+def mean(num1, num2):
+    mean_btw = (num1+num2)/2
+    return mean_btw
+
+
+"""def me(minimum, maximum):
+    median_number = mean(minimum, maximum)
+    simbolo = raw_input("Your number is " + str(median_number) + " ? ")
+    if simbolo == "+":
+        minimum = median_number-
+        me(minimum, maximum)
+    elif simbolo == "-":
+        maximum = median_number
+        me(minimum, maximum)
+    elif simbolo == "=":
+        print("Awesome! your number is " + str(median_number) + " !!")"""
